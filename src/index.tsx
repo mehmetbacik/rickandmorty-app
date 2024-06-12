a496359318222ba2bfce89e81fb0a8ee8237e881
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-06-12 19:14:30 +0300

    Update
    Updated for SASS.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
+import './styles/main.scss';
 import App from "./App";
 import { store } from "./app/store";
 import Home from "./pages/Home";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import "../styles/main.scss";
 
 const Home: React.FC = () => {
   return (
