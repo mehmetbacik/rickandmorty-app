054720371aea32715869283c66818d60712fcbc3
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-06-20 23:10:29 +0300

    Update
    Updated for HomeLocationComponent.

---
+++ src/components/Location.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../app/store";
+import { getLocations } from "../features/locations/locationsSlice";
+
+const Location: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { locations, loading, error } = useSelector(
+    (state: RootState) => state.locations
+  );
+  const [randomLocations, setRandomLocations] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (locations.length === 0) {
+      dispatch(getLocations({ page: 1, filters: {} }));
+    } else {
+      const shuffledLocations = [...locations].sort(() => Math.random() - 0.5);
+      setRandomLocations(shuffledLocations.slice(0, 4));
+    }
+  }, [dispatch, locations]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="random-locations">
+      <h2>Locations</h2>
+      {randomLocations.map((location) => (
+        <div key={location.id}>
+          <h3>{location.name}</h3>
+          <p>Type: {location.type}</p>
+          <p>Dimension: {location.dimension}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Location;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/main.scss";
 import SwiperSlider from "../components/SwiperSlider";
 import Episode from "../components/Episode";
+import Location from "../components/Location";
 
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
       <SwiperSlider />
       <Episode />
+      <Location />
     </div>
   );
 };
